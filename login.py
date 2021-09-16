1931e3fc76594f8d9ebe210869977566ce666f13
Your Name <theclassicsponge@gmail.com>
2021-09-16 18:10:19 +0100

    first

--- login.py
+++ login.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, redirect, url_for, g
 import psycopg2
 import psycopg2.extras
+import re
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'secret-key'
@@ -26,7 +28,20 @@ def forgot():
 @app.route("/create", methods=['GET', 'POST'])
 def create():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    # check
+
+    # check if "username", "password", "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'password' in request.form and 'email' in request.form:
+        # create variables for easy access
+        fullname = request.form['fullname']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        _hashed_password = generate_password_hash(password)
+
+        print(fullname)
+        print(_hashed_password)
+        print(username)
+        print(email)
     return render_template("create.html")
 
 
